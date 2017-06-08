4d00d69e3f6a117bdd77a31d8e596982be1c2ac2
Steven <sidiw1112@gmail.com>
2017-06-08 11:11:31 -0700

    Fixed water material missing

--- Assets/WaterContainer.cs
+++ Assets/WaterContainer.cs
@@ -3,25 +3,30 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class WaterContainer: MonoBehaviour {
+
+	[Tooltip("the first child object should be the water to make sure the script work.")]
 	private Transform waterInCup;
 	private float fillSpeed = 2f;
 	private float degreeToPoolWater = 90f;
+	private AudioSource[] waterSoundEffects;
 	float t  = 0;
 	const float emptyCupScale = 0.0001f;
 	const float fullCupScale = 1f;
 
 	// Use this for initialization
 	void Start () {
-		waterInCup = transform.GetChild (0);
-
+		waterInCup = GameObject.Find("WaterInCup").transform;
+		waterSoundEffects = GetComponents<AudioSource> ();
 	}
 
 	void Update(){
 		//When don't hold the cup in right rotation
-
-		if(Quaternion.Angle(Quaternion.identity,transform.rotation)>degreeToPoolWater){
-			if (waterInCup.localScale.y>=emptyCupScale) {
+		//Debug.Log(Quaternion.Angle(Quaternion.identity,transform.rotation));
+		if(Quaternion.Angle(Quaternion.identity,transform.rotation)>=degreeToPoolWater){
+			Debug.Log ("EmptyCup");
+			if (waterInCup.localScale.y>emptyCupScale) {
 				EmptyCup ();
+				//Debug.Log ("EmptyCup");
 			}
 			t = 0;
 		}
@@ -30,13 +35,13 @@ public class WaterContainer: MonoBehaviour {
 		
 		if(coll.tag == "Waterfall"){
 			//Fill the Cup
-			if (waterInCup.localScale.y<=fullCupScale) {
+			if (waterInCup.localScale.y<fullCupScale) {
 				FillCup ();
 			}
 			t = 0;
 		}else if(coll.tag == "Player"){
 			//Empty the Cup
-			if (waterInCup.localScale.y>=emptyCupScale) {
+			if (waterInCup.localScale.y>emptyCupScale) {
 				EmptyCup ();
 			}
 			t = 0;
@@ -48,6 +53,10 @@ public class WaterContainer: MonoBehaviour {
 		
 		t += fillSpeed * Time.deltaTime;
 		waterInCup.localScale = new Vector3 (waterInCup.localScale.x, Mathf.Lerp (waterInCup.localScale.y, emptyCupScale, t), waterInCup.localScale.z);
+		if (!waterSoundEffects [1].isPlaying) {
+			waterSoundEffects [1].Play();
+		} 
+
 
 	}
 
@@ -55,6 +64,8 @@ public class WaterContainer: MonoBehaviour {
 
 		t += fillSpeed * Time.deltaTime;
 		waterInCup.localScale = new Vector3 (waterInCup.localScale.x, Mathf.Lerp (waterInCup.localScale.y, fullCupScale, t), waterInCup.localScale.z);
-
+		if (!waterSoundEffects [0].isPlaying) {
+			waterSoundEffects [0].Play();
+		} 
 	}
 }
