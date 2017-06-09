1c7d7721de30d84ff33d93d067f289cd1f389e25
Steven <sidiw1112@gmail.com>
2017-06-08 17:08:55 -0700

    Added blur effect for eating posion mushroom, selection script for different mushrooms

---
+++ Assets/Script/MushroomSelection.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.ImageEffects;
+
+[RequireComponent(typeof(BlurOptimized))]
+public class MushroomSelection : MonoBehaviour
+{
+    public BlurOptimized blurEffect;
+
+    public void Start()
+    {
+        blurEffect.blur.enabled = false;
+    }
+
+    // Update is called once per frame
+    public void Update()
+    {
+    }
+
+    public void OnTriggerEnter(Collider col)
+    {
+        Debug.Log("Tag name:" + col.gameObject.tag);
+        if (string.Equals(col.gameObject.tag, "goodmushroom"))
+        {
+            // trigger unicorn and dizzy effect
+            // trigger sound
+            col.gameObject.SetActive(false);
+        }
+
+        if (string.Equals(col.gameObject.tag, "rainbowmushroom"))
+        {
+            // trigger rainbow effect
+            // trigger sound
+            col.gameObject.SetActive(false);
+        }
+
+        if (string.Equals(col.gameObject.tag, "posionmushroom"))
+        {
+            // trigger posion effect
+            // trigger sound
+            col.gameObject.SetActive(false);
+            blurEffect.blur.enabled = true;
+        }
+    }
+
+    public void OnCollisionEnter(Collision col)
+    {
+        Debug.Log("Tag name:" + col.gameObject.tag);
+        if (string.Equals(col.gameObject.tag, "goodmushroom"))
+        {
+            // trigger unicorn and dizzy effect
+            // trigger sound
+            col.gameObject.SetActive(false);
+        }
+
+        if (string.Equals(col.gameObject.tag, "rainbowmushroom"))
+        {
+            // trigger rainbow effect
+            // trigger sound
+            col.gameObject.SetActive(false);
+        }
+
+        if (string.Equals(col.gameObject.tag, "posionmushroom"))
+        {
+            // trigger posion effect
+            // trigger sound
+            col.gameObject.SetActive(false);
+            blurEffect.blur.enabled = true;
+        }
+    }
+}
